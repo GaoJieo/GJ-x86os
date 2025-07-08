b259f458e6027d8e64ce4125c0999f14db79f962
GaoJieo <lgj235@gmail.com>
2025-07-08 11:22:34 +0800

    初始化日志，定位输出到RS232端口，printf格式化输出

--- source/kernel/init/init.c
+++ source/kernel/init/init.c
@@ -7,6 +7,8 @@
 #include "cpu/irq.h"
 #include "dev/time.h"
 #include "os_cfg.h"
+#include "tools/log.h"
+#include "tools/klib.h"
 
 static boot_info_t * init_boot_info;        // 启动信息
 
@@ -19,12 +21,22 @@ void kernel_init (boot_info_t * boot_info) {
 
     // 初始化CPU，再重新加载
     cpu_init();
+
+    log_init();
     irq_init();
     time_init();
 }
 
 void init_main(void) {
-    //int a = 3 / 0;
+    
+    log_printf("Kernel is running....");
+    log_printf("Version: %s, name: %s", OS_VERSION, "tiny x86 os");
+    log_printf("%d %d %x %c", -123, 123456, 0x12345, 'a');
+
+    int a = 3;
+    ASSERT(a > 2);
+    ASSERT(a < 2);
+    // int b = 3 / 0;
     // irq_enable_global();
     for (;;) {}
 }

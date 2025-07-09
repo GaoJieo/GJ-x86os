fd030083d957928df57f48b2633779b97b0a5d16
GaoJieo <lgj235@gmail.com>
2025-07-09 21:01:00 +0800

    实现自定义双向链表，使用tss段任务切换机制

--- source/kernel/init/init.c
+++ source/kernel/init/init.c
@@ -10,6 +10,8 @@
 #include "os_cfg.h"
 #include "tools/log.h"
 #include "tools/klib.h"
+#include "tools/list.h"
+
 
 static boot_info_t * init_boot_info;        // 启动信息
 
@@ -29,7 +31,7 @@ void kernel_init (boot_info_t * boot_info) {
 }
 
 static task_t first_task;       // 第一个任务
-static uint32_t init_task_stack[1024];	// 空闲任务堆栈
+static uint32_t init_task_stack[800];	// 空闲任务堆栈
 static task_t init_task;
 
 /**
@@ -45,19 +47,82 @@ void init_task_entry(void) {
     }
 }
 
-void init_main(void) {
+
+void list_test (void) {
+    list_t list;
+    list_node_t nodes[5];
     
+    list_init(&list);
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+    // 插入
+    for (int i = 0; i < 5; i++) {
+        list_node_t * node = nodes + i;
+        log_printf("insert first to list: %d, 0x%x", i, (uint32_t)node);
+        list_insert_first(&list, node);
+    }
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+    list_init(&list);
+    for (int i = 0; i < 5; i++) {
+        list_node_t * node = nodes + i;
+        log_printf("insert last to list: %d, 0x%x", i, (uint32_t)node);
+        list_insert_last(&list, node);
+    }
+
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+    for (int i = 0; i < 5; i++) {
+        list_node_t * node = list_remove_first(&list);
+        log_printf("remove first from list: %d, 0x%x", i, (uint32_t)node);
+    }
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+
+    // 插入
+    for (int i = 0; i < 5; i++) {
+        list_node_t * node = nodes + i;
+        log_printf("insert last to list: %d, 0x%x", i, (uint32_t)node);
+        list_insert_last(&list, node);
+    }
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+    for (int i = 0; i < 5; i++) {
+        list_node_t * node = nodes + i;
+        log_printf("remove first from list: %d, 0x%x", i, (uint32_t)node);
+        list_remove(&list, node);
+    }
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+    struct type_t {
+        int i;
+        list_node_t node;
+    }v = {0x123456};
+
+    list_node_t * v_node = &v.node;
+    struct type_t * p = list_node_parent(v_node, struct type_t, node);
+    if (p->i != 0x123456) {
+        log_printf("error");
+    }
+ }
+
+
+void init_main(void) {
+
+    list_test();
+
     log_printf("Kernel is running....");
     log_printf("Version: %s, name: %s", OS_VERSION, "tiny x86 os");
     log_printf("%d %d %x %c", -123, 123456, 0x12345, 'a');
     
     // 初始化任务
-    task_init(&init_task, (uint32_t)init_task_entry, (uint32_t)&init_task_stack[1024]);
+    task_init(&init_task, (uint32_t)init_task_entry, (uint32_t)&init_task_stack[800]);
     task_init(&first_task, 0, 0);
     write_tr(first_task.tss_sel);
 
     //int a = 3 / 0;
     // irq_enable_global();
+    // first_task表示当前main的运行进程，他的stack会在首次调用swatich时初始化
     int count = 0;
     for (;;) {
         log_printf("first task: %d", count++);
